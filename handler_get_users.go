3a403a2e9878e370bd96c390f738e0907246802c
ItencY <itency@outlook.com>
2026-06-16 10:06:38 +0300

    feat: add users command and GetUsers SQL query
    - Add GetUsers query with :many tag to fetch all users
    - Generate Go code using sqlc
    - Implement 'users' CLI command to print the user list
    - Highlight the currently logged-in user with '(current)'
    - Run and verify CLI tests

---
+++ handler_get_users.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func handlerGetUsers(s *state, cmd command) error {
+	ctx := context.Background()
+	users, err := s.db.GetUsers(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get users: %v", err)
+	}
+	currentUser := s.cfg.CurrentUserName
+	for _, user := range users {
+		if user.Name == currentUser {
+			fmt.Printf("* %s (current)\n", user.Name)
+		} else {
+			fmt.Printf("* %s\n", user.Name)
+		}
+	}
+	return nil
+}
