e74f52b7780da89e2c23e79d50b32abc4ca8c1d3
ItencY <itency@outlook.com>
2026-06-18 08:31:02 +0300

    feat: add feeds table, sqlc generation, and addfeed CLI command
    - Create `feeds` table with unique url and user cascade delete
    - Add SQLC queries for feed creation
    - Add `addfeed` CLI command to link and print new feeds

---
+++ handler_addfeed.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/itency/blog_aggregator/internal/database"
+)
+
+func handlerAddFeed(s *state, cmd command) error {
+	if len(cmd.args) < 2 {
+		return errors.New("the addFeed handler expects a two argument, the name and url")
+	}
+	ctx := context.Background()
+	user, err := s.db.GetUser(ctx, s.cfg.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("Failed to retrieve the user: %v", err)
+	}
+	feed, err := s.db.CreateFeed(ctx, database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      user.Name,
+		Url:       cmd.args[1],
+		UserID:    user.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create feed: %v", err)
+	}
+	fmt.Println("Feed created successfully:")
+	fmt.Printf("* ID:            %s\n", feed.ID)
+	fmt.Printf("* Created:       %v\n", feed.CreatedAt)
+	fmt.Printf("* Updated:       %v\n", feed.UpdatedAt)
+	fmt.Printf("* Name:          %s\n", feed.Name)
+	fmt.Printf("* URL:           %s\n", feed.Url)
+	fmt.Printf("* UserID:        %s\n", feed.UserID)
+	fmt.Println("=====================================")
+	return nil
+}
