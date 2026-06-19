824b446200325269e2627e16eb6c469317252aba
ItencY <itency@outlook.com>
2026-06-19 11:22:43 +0300

    feat: enhance agg command with continuous RSS scraping loop
    - DB/Migration: Add nullable `last_fetched_at` column to `feeds` table.
    - SQL/Queries: Implement `MarkFeedFetched` and `GetNextFeedToFetch`
      using `NULLS FIRST`.
    - App/Scraper: Create `scrapeFeeds` function to fetch, parse, and log
      RSS items.
    - CLI/Commands: Refactor `agg` command to accept duration and run a
      `time.Ticker` loop.

--- handler_agg.go
+++ handler_agg.go
@@ -3,14 +3,57 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
+
+	"github.com/itency/blog_aggregator/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	ctx := context.Background()
-	res, err := fetchFeed(ctx, "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return fmt.Errorf("couldn't fetch feed: %w", err)
+	if len(cmd.args) < 1 || len(cmd.args) > 2 {
+		return fmt.Errorf("usage: %v <time_between_reqs>", cmd.name)
 	}
-	fmt.Printf("Feed: %+v\n", res)
-	return nil
+
+	timeBetweenRequests, err := time.ParseDuration(cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("invalid duration: %v", err)
+	}
+
+	log.Printf("Collecting feeds every %s...", timeBetweenRequests)
+
+	ticker := time.NewTicker(timeBetweenRequests)
+
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) {
+	ctx := context.Background()
+	feed, err := s.db.GetNextFeedToFetch(ctx)
+	if err != nil {
+		log.Println("Couldn't get next feeds to fetch", err)
+		return
+	}
+	log.Println("Found a feed to fetch!")
+	scrapeFeed(s.db, feed)
+}
+
+func scrapeFeed(db *database.Queries, feed database.Feed) {
+	ctx := context.Background()
+	_, err := db.MarkFeedFetched(ctx, feed.ID)
+	if err != nil {
+		log.Printf("Couldn't mark feed %s fetched: %v", feed.Name, err)
+		return
+	}
+
+	feedData, err := fetchFeed(ctx, feed.Url)
+	if err != nil {
+		log.Printf("Couldn't collect feed %s: %v", feed.Name, err)
+		return
+	}
+	for _, item := range feedData.Channel.Item {
+		fmt.Printf("Found post: %s\n", item.Title)
+	}
+	log.Printf("Feed %s collected, %v posts found", feed.Name, len(feedData.Channel.Item))
 }
